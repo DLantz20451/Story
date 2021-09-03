8d629db7203f8753638cba52741e212db6a86492
Dravin Lantz <20451@my4county.net>
2021-09-03 10:32:43 -0400

    working on story

--- src/story/Story.java
+++ src/story/Story.java
@@ -21,24 +21,35 @@ public class Story {
     public static void main(String[] args) {
         int number1 =0;
       double number2 =0;
-      int number3 =0;
+      double number3 =0;
       int number4 =0;
       int total =0;
       String firstName;
-      String charName;
+      String awnser;
       
       Scanner sc = new Scanner(System.in);
       
-      System.out.println("Hello there traveler, before you can continue, I need your name.");
+      System.out.println("Hello there traveler, before you can continue, I need to fill out your profile."
+              + "So first things first, Whats your name.");
         firstName = sc.nextLine();
       
-      System.out.println("Nice name , now I need your age if you can. ");
+      System.out.println("Nice name"+firstName , "now I need your age if you can. ");
       number1 = sc.nextInt();
       
-       System.out.println("One more thing, your hight. ");
-       number2 = sc.nextDouble();
+      System.out.println("Ok, one more thing, your hight. ");
+      number2 = sc.nextDouble();
        
-       System.out.print("Perfect, now that the paper work is done, lets see if yoou know basic");
+      System.out.print("Perfect, now that you profile is done, lets run you through some basic math, ok.");      
+      awnser = sc.nextLine();
+       
+      System.out.print("So whats 25 divided by 50? ");
+      number3 = sc.nextDouble();
+       
+      System.out.println("Nice. Ok, now whats 17+32-20");
+      number4 = sc.nextInt();
+       
+      System.out.println("Good job mate, now we just gotta get you armed. so what fighting style do you prefer.");
+       
     }
     
 }
