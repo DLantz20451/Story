e46c313afaaf4c0df221b05a779968b1ce38d7e1
Dravin Lantz <20451@my4county.net>
2021-09-10 09:31:10 -0400

    finished story

--- src/story/Story.java
+++ src/story/Story.java
@@ -24,53 +24,66 @@ public class Story {
       double number3 =0;
       int number4 =0;
       String firstName;
-      String awnser;
-      String rangedVsMelee;
-      String weaponsOfChoice;
-      String travelerTitle;
+      String color;
+      String snackOfChoice;
+      String drinkOfChoice;
+      String worstDrink;
+      int favNumber =0;
       
       Scanner sc = new Scanner(System.in);
       
-      System.out.println("Hello there traveler, before you can continue, I need to fill out your profile."
-              + "So first things first, Whats your name.");
-        firstName = sc.nextLine();
+      System.out.println(" Whats your name.");
+        firstName = sc.next();
       
-      System.out.println("Nice name " + firstName +", now I need your age if you can.");
+      System.out.println("Whats your age.");
       number1 = sc.nextInt();
       
-      System.out.println("Ok, one more thing, your hight. ");
+      System.out.println("Whats your hight. ");
       number2 = sc.nextDouble();
        
-      System.out.print("Perfect, now that you profile is done, lets run you through some basic math, ok. ");      
-      awnser = sc.nextLine();
-      sc.nextLine();
-              
+      System.out.print("whats a color you like ");      
+      color = sc.next();
+      
+      System.out.println("Whats your favorite number");
+       favNumber = sc.nextInt();
        
-      System.out.print("So whats 25 divided by 50? ");
+      System.out.print("Whats 25 divided by 50? ");
       number3 = sc.nextDouble();
        
-      System.out.println("Nice. Ok, now whats 17+32-20");
+      System.out.println("Whats 17+32-20");
       number4 = sc.nextInt();
-       
-      System.out.println("Good job mate, now we just gotta get you armed. So what fighting style do you prefer,"
-              + " ranged or melee");
-      sc.nextLine();
-      rangedVsMelee = sc.nextLine();
-       
-      System.out.println( "Hm " + rangedVsMelee + ", nice choice");
-      sc.nextLine();
       
-      System.out.println("Now what type of " + rangedVsMelee + " do you prefer");
-      weaponsOfChoice = sc.nextLine();
+      System.out.println("Whats your snack of choice");
+      snackOfChoice = sc.next();
       
-      System.out.println("A " + weaponsOfChoice + ", nice choice. Now what title will you give yourself");
-      travelerTitle = sc.nextLine();
+      System.out.println("Whats your prefered drink");
+      drinkOfChoice = sc.next();
       
-      System.out.println("" + firstName + " The " + travelerTitle + ". Well looks like you're all ready to make "
-              + "a name for yourself. Good luck my friend, if you ever need a hand you just give a shout ok");
-      awnser = sc.nextLine();
+      System.out.println("Whats a drink you'd want to advoid");
+      worstDrink = sc.next();
       
-      System.out.println("And so starts " + firstName + "'s Legendary story." );
+      
+      System.out.println("Dear School, this is " + firstName +", and I'm " + number1 + " and I'd "
+              + "like to know what is up with the vending machines.");
+      
+      System.out.println(" First of all it's an ugly brown color, why not have it " + color + 
+              " with a " + favNumber + " design, it would look so much better.");
+      
+      System.out.println(" Second, what is up with the food in there. Half of it is just unliked "
+              + "pop tart flavor. You could add in some liked snacks like " + snackOfChoice + ". ");
+      
+      System.out.println("Also what is up with the drinks, its all just water and disgusting " 
+              +worstDrink + " Why not add some " + drinkOfChoice + ".");
+      
+      System.out.println(" Thirdly, why is the so high up, like I'm " + number2 + ", and even I can't"
+              + " see the top section of the vending machine It has 50 rows each with 5 items."
+              + " We need to reduce that by " + number3 + ".");
+      
+      System.out.println(" Now while I know that you can replace the machine and get a knew one"
+              + ", I would be willing to donate " + number4 + " to help for the better decor one it.");
+      
+      System.out.println(" Thank you for your time and patience. " + firstName + "" );
+      
     }
     
 }
